8c8b7bcbb3f3d4660512911ff81ea72816ed89ed
Malte Wunsch <mw@webfactory.de>
2014-10-20 12:22:23 +0200

    Added a decorator to gracefully deal with exceptions.

---
+++ Tests/Translator/Formatting/GracefulExceptionsDecoratorTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Webfactory\TranslationBundle\Tests\Translator\Formatting;
+
+use Psr\Log\LoggerInterface;
+use Webfactory\IcuTranslationBundle\Translator\Formatting\GracefulExceptionsDecorator;
+use Webfactory\IcuTranslationBundle\Translator\Formatting\FormatterInterface;
+use Webfactory\IcuTranslationBundle\Translator\FormattingException;
+
+/**
+ * Tests for the decorator that deals gracefully with exceptions.
+ */
+final class GracefulExceptionsDecoratorTest extends \PHPUnit_Framework_TestCase 
+{
+    /**
+     * System under test.
+     *
+     * @var GracefulExceptionsDecorator
+     */
+    protected $decorator = null;
+
+    /**
+     * Mocked inner formatter.
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject|FormatterInterface
+     */
+    protected $innerFormatter = null;
+
+    /**
+     * Mocked injected logger.
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject|LoggerInterface
+     */
+    protected $logger = null;
+
+    /**
+     * Initializes the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->innerFormatter = $this->getMock('\Webfactory\IcuTranslationBundle\Translator\Formatting\FormatterInterface');
+        $this->logger         = $this->getMock('\Psr\Log\LoggerInterface');
+        $this->decorator      = new GracefulExceptionsDecorator($this->innerFormatter, $this->logger);
+    }
+
+    /**
+     * @test
+     * Checks if the decorator implements the formatter interface.
+     */
+    public function implementsFormatterInterface()
+    {
+        $expectedType = '\Webfactory\IcuTranslationBundle\Translator\Formatting\FormatterInterface';
+        $this->assertInstanceOf($expectedType, $this->decorator);
+    }
+
+    /**
+     * @test
+     * Checks the decorator returns the inner response if no exception got thrown.
+     */
+    public function returnInnerResponseNoExceptionGotThrown()
+    {
+        $innerResponse = 'inner response';
+        $this->innerFormatter->expects($this->once())
+                             ->method('format')
+                             ->will($this->returnValue($innerResponse));
+        $this->assertSame($innerResponse, $this->decorator->format('', '', array()));
+    }
+
+    /**
+     * @test
+     * Checks the decorator logs an exception thrown in the inner formatter.
+     */
+    public function logException()
+    {
+        $innerException = new FormattingException('en', 'msg_id', 'msg pattern', array(), new \Exception());
+        $this->innerFormatter->expects($this->once())
+                             ->method('format')
+                             ->will($this->throwException($innerException));
+        $this->logger->expects($this->once())
+                     ->method('error')
+                     ->with($this->equalTo($innerException));
+
+        $this->decorator->format('', '', array());
+    }
+
+    /**
+     * @test
+     * Checks the decorator gracefully returns a string if an exception is thrown in the inner formatter.
+     */
+    public function returnStringInCaseOfException()
+    {
+        $innerException = new FormattingException('en', 'msg_id', 'msg pattern', array(), new \Exception());
+        $this->innerFormatter->expects($this->once())
+                             ->method('format')
+                             ->will($this->throwException($innerException));
+
+        $this->assertInternalType('string', $this->decorator->format('', '', array()));
+    }
+
+    /**
+     * @test
+     * Checks the constructor parameter for the logger is optional, i.e. nothing breaks if it was not set and the inner
+     * formatter throws an exception.
+     */
+    public function loggerIsOptional()
+    {
+        $decorator = new GracefulExceptionsDecorator($this->innerFormatter);
+        $innerException = new FormattingException('en', 'msg_id', 'msg pattern', array(), new \Exception());
+        $this->innerFormatter->expects($this->once())
+                             ->method('format')
+                             ->will($this->throwException($innerException));
+
+        $this->setExpectedException(null);
+
+        $decorator->format('', '', array());
+    }
+}
---
+++ Translator/Formatting/GracefulExceptionsDecorator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Webfactory\IcuTranslationBundle\Translator\Formatting;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Webfactory\IcuTranslationBundle\Translator\FormattingException;
+
+/**
+ * Catches exceptions generated in the decorated formatter to log them and to returns a string gracefully.
+ *
+ * @final by default.
+ */
+final class GracefulExceptionsDecorator extends AbstractFormatterDecorator
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Creates a decorator for the provided formatter.
+     *
+     * @param \Webfactory\IcuTranslationBundle\Translator\Formatting\FormatterInterface $innerFormatter
+     * @param LoggerInterface $logger
+     */
+    public function __construct(FormatterInterface $innerFormatter, LoggerInterface $logger = null)
+    {
+        $this->innerFormatter = $innerFormatter;
+        $this->logger = ($logger !== null) ? $logger : new NullLogger();
+    }
+
+    /**
+     * Formats the provided message.
+     *
+     * @param string $locale
+     * @param string $message
+     * @param array(string=>mixed) $parameters
+     * @return string The formatted message.
+     */
+    public function format($locale, $message, array $parameters)
+    {
+        try {
+            return parent::format($locale, $message, $parameters);
+        } catch (FormattingException $e) {
+            $this->logger->error($e);
+            return $message . ' (formatting error)';
+        }
+    }
+}
