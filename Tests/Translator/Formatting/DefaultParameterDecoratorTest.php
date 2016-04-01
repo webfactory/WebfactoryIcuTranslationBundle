3b3220b2c0eb1603ecf9555b9ace0eb751aef80e
Matthias Molitor <mmo@webfactory.de>
2016-04-01 16:58:06 +0200

    added test case stub

---
+++ Tests/Translator/Formatting/DefaultParameterDecoratorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Webfactory\TranslationBundle\Tests\Translator\Formatting;
+
+class DefaultParameterDecoratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Initializes the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+    }
+
+    /**
+     * Cleans up the test environment.
+     */
+    protected function tearDown()
+    {
+
+        parent::tearDown();
+    }
+
+    public function testInjectsDefaultForAlphabeticPlaceholder()
+    {
+
+    }
+
+    public function testInjectsDefaultForAlphanumericPlaceholder()
+    {
+
+    }
+
+    public function testInjectsDefaultForNumericPlaceholder()
+    {
+
+    }
+
+    public function testInjectsDefaultIfPlaceholderIsUsedMultipleTimes()
+    {
+
+    }
+
+    public function testDoesNotSetDefaultIfParameterForAlphabeticPlaceholderIsPassed()
+    {
+
+    }
+
+    public function testDoesNotSetDefaultIfParameterForNumericPlaceholderIsPassed()
+    {
+
+    }
+}
