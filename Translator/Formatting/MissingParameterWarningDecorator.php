f3b6f72398eafb647b2d1b952049500eb48d3ef9
Matthias Molitor <mmo@webfactory.de>
2016-08-04 16:56:50 +0200

    implemented decorator

--- Translator/Formatting/MissingParameterWarningDecorator.php
+++ Translator/Formatting/MissingParameterWarningDecorator.php
@@ -4,6 +4,7 @@ namespace Webfactory\IcuTranslationBundle\Translator\Formatting;
 
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
+use Webfactory\IcuTranslationBundle\Translator\Formatting\Exception\FormattingException;
 
 /**
  * Decorator that generates a warning log entry whenever a parameter for a formatted message seems to be missing.
@@ -28,5 +29,37 @@ class MissingParameterWarningDecorator extends AbstractFormatterDecorator
     {
         parent::__construct($innerFormatter);
         $this->logger = ($logger !== null) ? $logger : new NullLogger();
+    }
+
+    /**
+     * Checks if all mentioned parameters are provided.
+     *
+     * @param string $locale
+     * @param string $message
+     * @param array(string=>mixed) $parameters
+     * @return string The formatted message.
+     */
+    public function format($locale, $message, array $parameters)
+    {
+        $pattern = '/\{(?P<variables>[a-zA-Z0-9_]+)/u';
+        preg_match_all($pattern, $message, $matches,  PREG_PATTERN_ORDER);
+        $usedParameters = $matches['variables'];
+        $availableParameters = array_keys($parameters);
+        $missingParameters = array_diff($usedParameters, $availableParameters);
+        if (count($missingParameters) > 0) {
+            $logMessage = 'The parameters %s are probably missing in the message "%s".';
+            $logMessage = sprintf($logMessage, implode(',', $missingParameters), $message);
+            $this->logger->error(
+                $logMessage,
+                array(
+                    'locale' => $locale,
+                    'message' => $message,
+                    'parameters' => $parameters,
+                    // Add an exception (but do not throw it) to ensure that we get a stack trace.
+                    'exception' => new FormattingException($logMessage)
+                )
+            );
+        }
+        return parent::format($locale, $message, $parameters);
     }
 }
