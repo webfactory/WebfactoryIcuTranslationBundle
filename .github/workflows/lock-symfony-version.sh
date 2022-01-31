8e6693bcd3b29c89868da48197e74eb1cb094347
Matthias Pigulla <mp@webfactory.de>
2022-01-31 16:32:19 +0000

    Don't pin symfony/translation-contracts

--- .github/workflows/lock-symfony-version.sh
+++ .github/workflows/lock-symfony-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat <<< $(jq --arg version $VERSION '.require  |= with_entries(if ((.key|test("^symfony/contracts")|not) and (.key|test("^symfony/"))) then .value=$version else . end)' < composer.json) > composer.json
+cat <<< $(jq --arg version $VERSION '.require  |= with_entries(if ((.key|test("^symfony/translation-contracts")|not) and (.key|test("^symfony/"))) then .value=$version else . end)' < composer.json) > composer.json
