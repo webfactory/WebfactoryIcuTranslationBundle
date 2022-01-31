5e1044fbc814c22d75141d07d8b910382b5c0764
mpdude <mpdude@users.noreply.github.com>
2022-01-31 11:12:04 +0000

    Fix CS with PHP-CS-Fixer 2.19.0

--- tests/Functional/TwigExtensionTest.php
+++ tests/Functional/TwigExtensionTest.php
@@ -44,7 +44,6 @@ class TwigExtensionTest extends KernelTestCase
         yield 'locale is used to format a number in the English locale' => ['3.141', '{{ "{param,number}" |icu_format({ param: 3.141 }, "en") }}'];
 
         yield 'using select expressions' => ['A was used', '{{ "{param, select, a {A was used} other {something else}}" |icu_format({ param: "a"}) }}'];
-
     }
 
     private function renderTemplate(string $template): string
